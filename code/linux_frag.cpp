54a28efcca81c521c8d5337b94b5e285181460c4
L4v <ivosevic.jovan@uns.ac.rs>
2021-11-24 10:17:58 +0100

    :rocket:Develop init with basic GLSL

--- code/linux_frag.cpp
+++ code/linux_frag.cpp
@@ -66,6 +66,26 @@ LoadAndCompileShader(std::string filename, GLuint shaderType) {
     return ShaderID;
 }
 
+internal u32
+CreateBasicProgram(u32 vShader, u32 fShader) {
+    u32 ProgramID = 0;
+    ProgramID = glCreateProgram();
+    glAttachShader(ProgramID, vShader);
+    glAttachShader(ProgramID, fShader);
+    glLinkProgram(ProgramID);
+
+    i32 Success;
+    char InfoLog[512];
+    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Success);
+    if(!Success) {
+        glGetProgramInfoLog(ProgramID, 512, NULL, InfoLog);
+        std::cerr << "[Err] Failed to link shader program:" << std::endl << InfoLog << std::endl;
+        return 0;
+    }
+
+    return ProgramID;
+}
+
 i32
 main() {
 
@@ -93,12 +113,62 @@ main() {
     glEnable(GL_CULL_FACE);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+    r32 Vertices[] = {
+        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f
+    };
+
+    u32 Indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    u32 VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(r32), (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(r32), (void*)(3 * sizeof(r32)));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    u32 BasicProgram, VShaderID, FShaderID;
+    VShaderID = LoadAndCompileShader("../code/shaders/basic.vs", GL_VERTEX_SHADER);
+    FShaderID = LoadAndCompileShader("../code/shaders/basic.fs", GL_FRAGMENT_SHADER);
+    BasicProgram = CreateBasicProgram(VShaderID, FShaderID);
+    glDetachShader(BasicProgram, VShaderID);
+    glDetachShader(BasicProgram, FShaderID);
+    glDeleteShader(VShaderID);
+    glDeleteShader(FShaderID);
+
+
     while(!glfwWindowShouldClose(Window)) {
         glfwGetFramebufferSize(Window, &G_WWIDTH, &G_WHEIGHT);
         r32 AspectRatio = G_WWIDTH / (float) G_WHEIGHT;
         glViewport(0, 0, G_WWIDTH, G_WHEIGHT);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(BasicProgram); 
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, sizeof(Indices), GL_UNSIGNED_INT, 0);
+       
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
 
         glfwSwapBuffers(Window);
         glfwPollEvents();
